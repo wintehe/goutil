50bb8378a9309b98b3647984c2910a21cf0d9e2f
h1339318006h@163.com <h1339318006h@163.com>
2023-02-28 18:43:40 +0800

    test-1

---
+++ utils/util.go
@@ -0,0 +1,19 @@
+package utils
+
+func intersect(a []string, b []string) []string {
+	inter := make([]string, 0)
+	mp := make(map[string]bool)
+
+	for _, s := range a {
+		if _, ok := mp[s]; !ok {
+			mp[s] = true
+		}
+	}
+	for _, s := range b {
+		if _, ok := mp[s]; ok {
+			inter = append(inter, s)
+		}
+	}
+
+	return inter
+}
